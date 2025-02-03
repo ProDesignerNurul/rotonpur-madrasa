b7265508348dd5951464e6d4c75d3d1a1a34bbb7
ProDesignerNurul <maxnewsbd@gmail.com>
2025-02-03 09:28:15 +0600

    some changes in our classes section

--- src/pages/OurClasses/OurClasses.jsx
+++ src/pages/OurClasses/OurClasses.jsx
@@ -7,8 +7,8 @@ const OurClasses = () => {
       <MainHeading heading="আমাদের বিভাগ সমূহ" />
       <div className="md:flex">
         <div className="md:w-1/2 px-4 md:px-8">
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" defaultChecked />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">হেদায়াতুন নাহু</div>
             <div className="collapse-content text-sm">
               মাদ্রাসা মূলত মুসলমানদের অধ্যয়ন-গবেষণা প্রতিষ্ঠান। সাধারণ অর্থে
@@ -17,8 +17,8 @@ const OurClasses = () => {
               অভিহিত। ফোরকানিয়া শব্দের মূল ফুরকান যার অর্থ বিশিষ্ট।
             </div>
           </div>
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">নাহবেমীর</div>
             <div className="collapse-content text-sm">
               মাদ্রাসা মূলত মুসলমানদের অধ্যয়ন-গবেষণা প্রতিষ্ঠান। সাধারণ অর্থে
@@ -27,8 +27,8 @@ const OurClasses = () => {
               অভিহিত। ফোরকানিয়া শব্দের মূল ফুরকান যার অর্থ বিশিষ্ট।
             </div>
           </div>
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">মীযানুস সরফ</div>
             <div className="collapse-content text-sm">
               মাদ্রাসা মূলত মুসলমানদের অধ্যয়ন-গবেষণা প্রতিষ্ঠান। সাধারণ অর্থে
@@ -37,8 +37,8 @@ const OurClasses = () => {
               অভিহিত। ফোরকানিয়া শব্দের মূল ফুরকান যার অর্থ বিশিষ্ট।
             </div>
           </div>
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">মক্তব পাঞ্জম</div>
             <div className="collapse-content text-sm">
               মাদ্রাসা মূলত মুসলমানদের অধ্যয়ন-গবেষণা প্রতিষ্ঠান। সাধারণ অর্থে
@@ -47,8 +47,8 @@ const OurClasses = () => {
               অভিহিত। ফোরকানিয়া শব্দের মূল ফুরকান যার অর্থ বিশিষ্ট।
             </div>
           </div>
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">মক্তব চাহারাম</div>
             <div className="collapse-content text-sm">
               মাদ্রাসা মূলত মুসলমানদের অধ্যয়ন-গবেষণা প্রতিষ্ঠান। সাধারণ অর্থে
@@ -59,8 +59,8 @@ const OurClasses = () => {
           </div>
         </div>
         <div className="md:w-1/2 px-4 md:px-8">
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" defaultChecked />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">
               নূরানী তৃতীয় শ্রেণী
             </div>
@@ -71,8 +71,8 @@ const OurClasses = () => {
               অভিহিত। ফোরকানিয়া শব্দের মূল ফুরকান যার অর্থ বিশিষ্ট।
             </div>
           </div>
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">
               নূরানী দ্বিতীয় শ্রেণী
             </div>
@@ -83,8 +83,8 @@ const OurClasses = () => {
               অভিহিত। ফোরকানিয়া শব্দের মূল ফুরকান যার অর্থ বিশিষ্ট।
             </div>
           </div>
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">
               নূরানী প্রথম শ্রেণী
             </div>
@@ -95,21 +95,11 @@ const OurClasses = () => {
               অভিহিত। ফোরকানিয়া শব্দের মূল ফুরকান যার অর্থ বিশিষ্ট।
             </div>
           </div>
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" />
+          <div className="collapse bg-base-100 border border-base-300">
+            <input type="checkbox" />
             <div className="collapse-title font-semibold">
               নূরানী শিশু শ্রেণী
             </div>
-            <div className="collapse-content text-sm">
-              মাদ্রাসা মূলত মুসলমানদের অধ্যয়ন-গবেষণা প্রতিষ্ঠান। সাধারণ অর্থে
-              মাদ্রাসা হচ্ছে আরবি ভাষা ও ইসলামি বিষয়ে অধ্যয়নের প্রতিষ্ঠান।
-              মাদ্রাসার প্রাথমিক স্তর মক্তব, নূরানি বা ফোরকানিয়া মাদ্রাসা নামে
-              অভিহিত। ফোরকানিয়া শব্দের মূল ফুরকান যার অর্থ বিশিষ্ট।
-            </div>
-          </div>
-          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-2" />
-            <div className="collapse-title font-semibold">মক্তব চাহারাম</div>
             <div className="collapse-content text-sm">
               মাদ্রাসা মূলত মুসলমানদের অধ্যয়ন-গবেষণা প্রতিষ্ঠান। সাধারণ অর্থে
               মাদ্রাসা হচ্ছে আরবি ভাষা ও ইসলামি বিষয়ে অধ্যয়নের প্রতিষ্ঠান।
