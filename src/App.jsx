2d42a2f44eee496abed3761c4288cb851a7dd2b3
ProDesignerNurul <maxnewsbd@gmail.com>
2025-02-11 00:38:52 +0600

    add pre loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,31 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import NavBar from "./pages/shared/NavBar";
 import Footer from "./pages/shared/Footer";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <div>
-      <NavBar />
-      <div>
-        <Home />
-      </div>
-      <Footer />
+    <div className="min-h-screen flex justify-center items-center">
+      {loading ? (
+        <span className="loading loading-bars loading-xl"></span>
+      ) : (
+        <div>
+          <NavBar />
+          <div>
+            <Home />
+          </div>
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
